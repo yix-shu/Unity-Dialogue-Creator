7d17dbb63efdcb3aac1eac340e9ab2553df2ee72
Yixing Xu <yix.shu723@gmail.com>
2024-04-25 01:05:29 -0400

    initial commit

---
+++ Assets/Editor/DialogueCreatorTool.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+
+public class DialogueCreatorTool : EditorTool
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
